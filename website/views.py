17bda46f7d793bbb249089daa77ec627de0cc593
SupunLiyanage88 <liyanagesupun10@gmail.com>
2025-01-20 23:19:10 +0530

    fix

--- website/views.py
+++ website/views.py
@@ -6,27 +6,43 @@ import json
 
 views = Blueprint('views', __name__)
 
-
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
     if request.method == 'POST': 
-        note = request.form.get('note')#Gets the note from the HTML 
+        note = request.form.get('note')
 
         if len(note) < 1:
             flash('Note is too short!', category='error') 
         else:
-            new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note 
-            db.session.add(new_note) #adding the note to the database 
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
             db.session.commit()
             flash('Note added!', category='success')
 
     return render_template("home.html", user=current_user)
 
+@views.route('/edit-note', methods=['POST'])
+@login_required
+def edit_note():
+    try:
+        note_data = json.loads(request.data)
+        note_id = note_data['noteId']
+        new_content = note_data['content']
+        
+        note = Note.query.get(note_id)
+        if note and note.user_id == current_user.id:
+            note.data = new_content
+            db.session.commit()
+            return jsonify({"success": True})
+        
+        return jsonify({"success": False, "error": "Note not found"}), 404
+    except Exception as e:
+        return jsonify({"success": False, "error": str(e)}), 500
 
 @views.route('/delete-note', methods=['POST'])
 def delete_note():  
-    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
+    note = json.loads(request.data)
     noteId = note['noteId']
     note = Note.query.get(noteId)
     if note:
